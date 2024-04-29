d67621a4f022eb974bebb1853b1ae15fd8dc7898
Davide <davidedesantis1992@gmail.com>
2024-04-29 12:24:34 +0200

    paginazione e burger

--- src/app/componenti/headeraccesso/headeraccesso.component.ts
+++ src/app/componenti/headeraccesso/headeraccesso.component.ts
@@ -4,12 +4,41 @@ import { AuthService } from '../../auth/auth.service';
 @Component({
   selector: 'app-headeraccesso',
   templateUrl: './headeraccesso.component.html',
-  styleUrl: './headeraccesso.component.css'
+  styleUrls: ['./headeraccesso.component.css']
 })
 export class HeaderaccessoComponent {
+
+  flagDropdown: boolean = false;
+
+  isRotated: boolean = false;
+
   constructor(private auth : AuthService) {}
   logout(){
     // localStorage.removeItem('access_token');
     this.auth.removeAuthenticated();
+  }
+
+  
+
+
+  apriChiudiDropdown() {
+    console.log("apriChiudiDropdown");
+    this.isRotated = !this.isRotated;
+    // Gestione apertura e chiusura dropdown pagerChanger
+    if (this.flagDropdown === false) {
+      // Apri pagerChanger e porta la flag a true
+      const dropdown = document.getElementById('pagerChanger');
+      if (dropdown) {
+        dropdown.classList.add('show');
+        this.flagDropdown = true;
+      }
+    } else {
+      // Chiudi pagerChanger e porta la flag a false
+      const dropdown = document.getElementById('pagerChanger');
+      if (dropdown) {
+        dropdown.classList.remove('show');
+        this.flagDropdown = false;
+      }
+    }
   }
 }
